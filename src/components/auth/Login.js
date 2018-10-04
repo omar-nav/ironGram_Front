43c63941ae21fe84bb20a9660f15b7ce9a091345
omar-nav <omar.navarro@g.austincc.edu>
2018-10-04 16:16:19 -0500

    login con tokens

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,69 +1,71 @@
-import React, {Component} from 'react'
-import {Input, Button} from 'antd'
+import React, { Component } from 'react'
+import { Input, Button } from 'antd'
 import axios from 'axios'
 import toastr from 'toastr'
 
 const url = 'http://localhost:3000/login'
 
-class Login extends Component{
+class Login extends Component {
 
     state = {
-        auth:{},
-        loading:false
+        auth: {},
+        loading: false
     }
 
     login = (e) => {
-        this.setState({loading:true})
+        this.setState({ loading: true })
         e.preventDefault()
-        const {auth} = this.state
+        const { auth } = this.state
         axios.post(url, auth)
-        .then(res=>{
-            console.log(res)
-            toastr.success("Te logueate!")
-            localStorage.setItem('user', JSON.stringify(res.data))
-            this.setState({loading:false})
-            const bonito = this.props.history
-            bonito.push('/profile')
+            .then(res => {
+                console.log(res)
+                toastr.success("Te logueate!")
+                localStorage.setItem('user', JSON.stringify(res.data.user))
+                localStorage.setItem('token', res.data.token)
 
-        })
-        .catch(e=>{
-            toastr.error("no quiero tu cochinada")
-            this.setState({loading:false})
-            
-        })
+                this.setState({ loading: false })
+                const bonito = this.props.history
+                bonito.push('/profile')
+
+            })
+            .catch(e => {
+                toastr.error("no quiero tu cochinada")
+                this.setState({ loading: false })
+
+            })
     }
 
     onChange = (e) => {
         const field = e.target.name
         const value = e.target.value
-        const {auth} = this.state
+        const { auth } = this.state
         auth[field] = value
-        this.setState({auth})
+        this.setState({ auth })
     }
 
-    render(){
-        const {auth, loading} = this.state
-        return(
-            <form onSubmit={this.login} style={{width:600, margin:"0 auto", padding:20}}>
+    render() {
+        const { auth, loading } = this.state
+        return (
+            <form onSubmit={this.login} style={{ width: 600, margin: "0 auto", padding: 20 }}>
                 <h2>Inicia sesión mijo</h2>
                 <p>
-                <Input 
-                    name="email"
-                    type="email"
-                    onChange={this.onChange}
-                    value={auth.email}
-                    placeholder="Tu correo" 
-                    />    
-                </p> 
+                    <Input
+                        name="email"
+                        type="email"
+                        onChange={this.onChange}
+                        value={auth.email}
+                        placeholder="Tu correo"
+                    />
+                </p>
                 <p>
-                <Input 
-                    name="password"
-                    type="password"
-                    onChange={this.onChange}
-                    value={auth.password}
-                    placeholder="Tu Password" 
-                    />    
-                </p>   
+                    <Input
+                        name="password"
+                        type="password"
+                        onChange={this.onChange}
+                        value={auth.password}
+                        placeholder="Tu Password"
+                    />
+                </p>
                 <Button loading={loading} type="primary" htmlType="submit" >Inicia sesión</Button>
             </form>
         )
--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -1,27 +1,40 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import logo from '../../logo.svg'
+import axios from 'axios'
+import toastr from 'toastr'
 
-
-class Profile extends Component{
+class Profile extends Component {
 
     state = {
-        user:{}
+        user: {}
     }
 
-    componentWillMount(){
+    componentWillMount() {
         const user = JSON.parse(localStorage.getItem('user'))
-        if(!user) return this.props.history.push('/login')
-        this.setState({user})
+        if (!user) return this.props.history.push('/login')
+        this.setState({ user })
     }
 
-    render(){
-        const {user} = this.state
-        return(
+    getPrivateInfo = () => {
+        axios.get('http://localhost:3000/private', {
+            headers: {
+                "Authorization": localStorage.getItem('token')
+            }
+        })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(e => toastr.error("algo fallo", e.message))
+    }
+
+    render() {
+        const { user } = this.state
+        return (
             <div>
-                <img style={{borderRadius:'50%'}} src={user.photoURL || logo} width="200" alt="user"/>
+                <img style={{ borderRadius: '50%' }} src={user.photoURL || logo} width="200" alt="user" />
                 <h1>{user.username}</h1>
                 <p>{user.email}</p>
-
+                <button onClick={this.getPrivateInfo}> bajate mi pack privado ;)</button>
             </div>
         )
     }
